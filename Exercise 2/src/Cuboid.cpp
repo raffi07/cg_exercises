a13be6554f4d3138f6edc505eb1845fb09f7c045
Raphael Imfeld <raphael.imfeld@uzh.ch>
2021-03-11 12:43:38 +0100

    Exercise 2 base task

---
+++ Exercise 2/src/Cuboid.cpp
@@ -0,0 +1,20 @@
+#include "Cuboid.h"
+#include <iostream>
+
+// this define and include will enable M_PI from math.h.  
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+namespace cgCourse
+{
+	Cuboid::Cuboid() : Shape()
+	{
+        /* TODO: add you code here
+
+        */
+
+        // for the moment we don't add normals here that is why we will not set the normals or face normals.
+	}
+ 
+
+}
---
+++ Exercise 2/src/Cuboid.h
@@ -0,0 +1,16 @@
+#ifndef Example2_Cuboid_h
+#define Example2_Cuboid_h
+
+#include "Shape.h"
+
+namespace cgCourse
+{
+	class Cuboid : public Shape
+	{
+	public:
+		Cuboid();
+
+	};
+}
+
+#endif
